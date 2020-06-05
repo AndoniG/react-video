86b66e0e5eb6e7f8d3a1389c536c5d0089b543df
Andoni G <andgonzalez75@gmail.com>
2020-06-05 10:27:11 -0500

    Project Base

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  // Archivo principal del programa
+  entry: './src/index.js',
+  // Dónde se guarda el output del build
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    // Que archivos se usan para el proyecto
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        // Identificación de archivos JS
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        // Identificación de archivos HTML
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+      {
+        // Identificación de archivos CSS
+        test: /\.(s*)css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    // Plugin que genera un HTML que incluya todos los bundles generados por Webpack usando scripts js
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    }),
+  ],
+};
